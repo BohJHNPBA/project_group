a1b0fa9e200d7eca52e27e3e7b774fe81a099435
BohJHNPBA <s10241797@connect.np.edu.sg>
2024-01-29 17:07:59 +0800

    Explanations + Testing to be completed when codes are finished

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cash_on_hand, overheads, profit_loss
 
 def main():
 
-    overheads_output = overheads.????()
+    overheads_output = overheads.overhead_function()
     coh_output = cash_on_hand.coh_function()
     profitloss_output = profit_loss.profit_loss_function()
 
@@ -11,4 +11,4 @@ def main():
 (overheads_output,coh_output,profitloss_output) = main()
 
 with open('summary_report.txt', 'w') as file:
-    file.write(overhead_output + coh_output + profitloss_output)
+    file.write(overheads_output + coh_output + profitloss_output)
